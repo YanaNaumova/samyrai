a5622f012435ab7218768d43146ca83618c687b9
Yana Naumova <naumovayana87@gmail.com>
2021-08-24 22:37:07 +0300

    21

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css'
 
 export const Dialogs=(props)=>{
   return(
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+      <div className={s.dialog+' '+s.active}>
+        Dimych
+      </div>
+        <div className={s.dialog}>
+          Andrew
+        </div>
+        <div className={s.dialog}>
+          Sveta
+        </div>
+        <div className={s.dialog}>
+          Valera
+        </div>
+      </div>
+        <div className={s.messages}>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>How is your it-kamasutra</div>
+          <div className={s.message}>Yo</div>
+      </div>
     </div>
   )
 }
